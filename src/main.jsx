4b0b7f030fb8af900519dc7d484daed740ed0c17
absra47 <absragirma630@gmail.com>
2024-01-05 12:36:25 +0300

    Initialize_react

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import "fontsoure/outfit";
-import "fontsoure/roboto";
+import "@fontsource/outfit";
+import "@fontsource/roboto";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
